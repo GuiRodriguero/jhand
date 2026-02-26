29096b6e042761cca3552e2eede2923ba07c705f
GuiRodriguero <gui.rodriguero@hotmail.com>
2026-02-26 01:00:14 -0300

    Add logic to get player's position

---
+++ src/main/java/com/gui/jhand/hand/Position.java
@@ -0,0 +1,7 @@
+package com.gui.jhand.hand;
+
+public enum Position {
+
+    BTN, SB, BB, CO, MP, UTG, UNKNOWN
+
+}
--- src/test/java/com/gui/jhand/hand/HandUtilsTest.java
+++ src/test/java/com/gui/jhand/hand/HandUtilsTest.java
@@ -3,6 +3,13 @@ package com.gui.jhand.hand;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.gui.jhand.hand.HandTemplateLoader.*;
+import static com.gui.jhand.hand.HandTemplateLoader.heroAtCO;
+import static com.gui.jhand.hand.HandTemplateLoader.heroAtMP;
+import static com.gui.jhand.hand.HandTemplateLoader.heroAtMPWith8Players;
+import static com.gui.jhand.hand.HandTemplateLoader.heroAtUTG;
+import static com.gui.jhand.hand.Position.*;
+import static com.gui.jhand.hand.Position.UTG;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class HandUtilsTest {
@@ -15,7 +22,7 @@ class HandUtilsTest {
 
 	@BeforeEach
 	void setUp() {
-		handAsString = HandTemplateLoader.valid();
+		handAsString = HandTemplateLoader.validAsString();
 		hand = Hand.of(handAsString, heroName);
 	}
 
@@ -37,6 +44,37 @@ class HandUtilsTest {
 	@Test
 	void should_check_pfr() {
 		assertThat(HandUtils.isPfr(handAsString, heroName)).isEqualTo(hand.isPfr());
+	}
+
+	@Test
+	void should_get_position_btn() {
+		assertThat(HandUtils.getPosition(heroAtBtn(), heroName)).isEqualTo(BTN);
+	}
+
+	@Test
+	void should_get_position_sb() {
+		assertThat(HandUtils.getPosition(heroAtSB(), heroName)).isEqualTo(SB);
+	}
+
+	@Test
+	void should_get_position_bb() {
+		assertThat(HandUtils.getPosition(heroAtBB(), heroName)).isEqualTo(BB);
+	}
+
+	@Test
+	void should_get_position_mp() {
+		assertThat(HandUtils.getPosition(heroAtMP(), heroName)).isEqualTo(MP);
+		assertThat(HandUtils.getPosition(heroAtMPWith8Players(), heroName)).isEqualTo(MP);
+	}
+
+	@Test
+	void should_get_position_co() {
+		assertThat(HandUtils.getPosition(heroAtCO(), heroName)).isEqualTo(CO);
+	}
+
+	@Test
+	void should_get_position_utg() {
+		assertThat(HandUtils.getPosition(heroAtUTG(), heroName)).isEqualTo(UTG);
 	}
 
 }
