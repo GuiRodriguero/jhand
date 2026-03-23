077a4e5020bf80f2354a37abc748c4182901b635
GuiRodriguero <gui.rodriguero@hotmail.com>
2026-03-23 02:06:14 -0300

    Add first endpoint and web Configuration

---
+++ src/main/java/com/gui/jhand/hand/HandImportRestService.java
@@ -0,0 +1,37 @@
+package com.gui.jhand.hand;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.springframework.http.ResponseEntity.noContent;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/v1/hands")
+public class HandImportRestService {
+
+	private final HandImportService importService;
+
+	@PostMapping("/import/batch")
+	public ResponseEntity<?> importHandHistories(@RequestParam("files") MultipartFile[] files,
+			@RequestParam("heroName") String heroName) throws IOException {
+		for (MultipartFile file : files) {
+			if (file.isEmpty()) {
+				continue;
+			}
+
+			importService.saveAllHandResultsFromFile(new String(file.getBytes(), UTF_8), heroName);
+		}
+
+		return noContent().build();
+	}
+
+}
---
+++ src/test/java/com/gui/jhand/hand/HandImportRestServiceTest.java
@@ -0,0 +1,39 @@
+package com.gui.jhand.hand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static com.gui.jhand.hand.HandTemplateLoader.validWonWithShowdown;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HandImportRestService.class)
+class HandImportRestServiceTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockitoBean
+	private HandImportService importService;
+
+	@Test
+	void should_import_hand_histories() throws Exception {
+		MockMultipartFile fakeFile = new MockMultipartFile("files", "hand_history.txt", "text/plain",
+				validWonWithShowdown().getBytes());
+
+		String heroName = "GuiRodri2013";
+
+		mockMvc.perform(multipart("/v1/hands/import/batch").file(fakeFile).param("heroName", heroName))
+			.andExpect(status().isNoContent());
+
+		verify(importService).saveAllHandResultsFromFile(anyString(), eq(heroName));
+	}
+
+}
