8594320708a96e2761957b9bb8d5f1ac9160cc0a
GuiRodriguero <gui.rodriguero@hotmail.com>
2026-03-25 01:10:41 -0300

    Refactor PokerStarsParsing rules to deal with real money

--- src/main/java/com/gui/jhand/parser/PokerStarsParsingRuleFactory.java
+++ src/main/java/com/gui/jhand/parser/PokerStarsParsingRuleFactory.java
@@ -28,32 +28,33 @@ class PokerStarsParsingRuleFactory {
 		rules.add(new ParsingRule(Pattern.compile("^Seat (\\d+): (.*?) \\(.*?in chips.*?\\)"), (m, ctx, line) -> List
 			.of(Action.of(SEAT_INFO, ctx.getCurrentStreet(), m.group(2), m.group(1), 0, line))));
 
-		rules.add(new ParsingRule(Pattern.compile("^(.*?): posts the ante (\\d+)"),
+		rules.add(new ParsingRule(Pattern.compile("^(.*?): posts the ante (\\$?)([\\d.]+)"),
 				(m, ctx, line) -> List.of(Action.of(POST_ANTE, ctx.getCurrentStreet(), m.group(1),
-						"posts the ante " + m.group(2), Double.parseDouble(m.group(2)), line))));
+						"posts the ante " + m.group(2) + m.group(3), Double.parseDouble(m.group(3)), line))));
 
-		rules.add(new ParsingRule(Pattern.compile("^(.*?): posts small blind (\\d+)"),
+		rules.add(new ParsingRule(Pattern.compile("^(.*?): posts small blind (\\$?)([\\d.]+)"),
 				(m, ctx, line) -> List.of(Action.of(POST_SMALL_BLIND, ctx.getCurrentStreet(), m.group(1),
-						"posts small blind " + m.group(2), Double.parseDouble(m.group(2)), line))));
+						"posts small blind " + m.group(2) + m.group(3), Double.parseDouble(m.group(3)), line))));
 
-		rules.add(new ParsingRule(Pattern.compile("^(.*?): posts big blind (\\d+)"),
+		rules.add(new ParsingRule(Pattern.compile("^(.*?): posts big blind (\\$?)([\\d.]+)"),
 				(m, ctx, line) -> List.of(Action.of(POST_BIG_BLIND, ctx.getCurrentStreet(), m.group(1),
-						"posts big blind " + m.group(2), Double.parseDouble(m.group(2)), line))));
+						"posts big blind " + m.group(2) + m.group(3), Double.parseDouble(m.group(3)), line))));
 
 		rules.add(new ParsingRule(Pattern.compile("^Dealt to (.*?) \\[(.*?)]"), (m, ctx, line) -> List
 			.of(Action.of(DEALT_TO_HERO, ctx.getCurrentStreet(), m.group(1), m.group(2), 0, line))));
 
-		rules.add(new ParsingRule(Pattern.compile("^(.*?): calls (\\d+)(?: and is all-in)?"),
+		rules.add(new ParsingRule(Pattern.compile("^(.*?): calls (\\$?)([\\d.]+)(?: and is all-in)?"),
 				(m, ctx, line) -> List.of(Action.of(ACTION_CALL, ctx.getCurrentStreet(), m.group(1),
-						"calls " + m.group(2), Double.parseDouble(m.group(2)), line))));
+						"calls " + m.group(2) + m.group(3), Double.parseDouble(m.group(3)), line))));
 
-		rules.add(new ParsingRule(Pattern.compile("^(.*?): raises (\\d+) to (\\d+)"),
+		rules.add(new ParsingRule(Pattern.compile("^(.*?): raises (\\$?)([\\d.]+) to (\\$?)([\\d.]+)"),
 				(m, ctx, line) -> List.of(Action.of(ACTION_RAISE, ctx.getCurrentStreet(), m.group(1),
-						"raises " + m.group(2) + " to " + m.group(3), Double.parseDouble(m.group(3)), line))));
+						"raises " + m.group(2) + m.group(3) + " to " + m.group(4) + m.group(5),
+						Double.parseDouble(m.group(5)), line))));
 
-		rules.add(new ParsingRule(Pattern.compile("^(.*?): bets \\$?([\\d.]+)(?: and is all-in)?"),
+		rules.add(new ParsingRule(Pattern.compile("^(.*?): bets (\\$?)([\\d.]+)(?: and is all-in)?"),
 				(m, ctx, line) -> List.of(Action.of(ACTION_BET, ctx.getCurrentStreet(), m.group(1),
-						"bets " + m.group(2), Double.parseDouble(m.group(2)), line))));
+						"bets " + m.group(2) + m.group(3), Double.parseDouble(m.group(3)), line))));
 
 		rules.add(new ParsingRule(Pattern.compile("^\\*\\*\\* FLOP \\*\\*\\* \\[(.*?)]"), (m, ctx, line) -> {
 			ctx.changeCurrentStreetTo(FLOP);
@@ -74,11 +75,13 @@ class PokerStarsParsingRuleFactory {
 				(m, ctx, line) -> List.of(Action.of(CARDS_SHOWN, ctx.getCurrentStreet(), m.group(1),
 						"shows [" + m.group(2) + "] " + m.group(3), 0, line))));
 
-		rules.add(new ParsingRule(Pattern.compile("^(.*?) collected (\\d+) from (?:(.*) )?pot"), (m, ctx, line) -> {
-			String potType = m.group(3) == null ? "" : m.group(3) + " ";
-			return List.of(Action.of(COLLECTED_POT, ctx.getCurrentStreet(), m.group(1),
-					"collected " + m.group(2) + " from " + potType + "pot", Double.parseDouble(m.group(2)), line));
-		}));
+		rules.add(new ParsingRule(Pattern.compile("^(.*?) collected (\\$?)([\\d.]+) from (?:(.*) )?pot"),
+				(m, ctx, line) -> {
+					String potType = m.group(4) == null ? "" : m.group(4) + " ";
+					return List.of(Action.of(COLLECTED_POT, ctx.getCurrentStreet(), m.group(1),
+							"collected " + m.group(2) + m.group(3) + " from " + potType + "pot",
+							Double.parseDouble(m.group(3)), line));
+				}));
 
 		rules.add(new ParsingRule(Pattern.compile("^\\*\\*\\* SUMMARY \\*\\*\\*"), (m, ctx, line) -> {
 			ctx.changeCurrentStreetTo(SUMMARY);
@@ -88,13 +91,13 @@ class PokerStarsParsingRuleFactory {
 		rules.add(new ParsingRule(Pattern.compile("^(.*?): folds"), (m, ctx, line) -> List
 			.of(Action.of(ACTION_FOLD, ctx.getCurrentStreet(), m.group(1), "folds", 0, line))));
 
-		rules.add(new ParsingRule(Pattern.compile("^Uncalled bet \\((\\d+)\\) returned to (.*)"),
-				(m, ctx, line) -> List.of(Action.of(UNCALLED_BET_RETURNED, ctx.getCurrentStreet(), m.group(2),
-						m.group(1), Double.parseDouble(m.group(1)), line))));
+		rules.add(new ParsingRule(Pattern.compile("^Uncalled bet \\((\\$?)([\\d.]+)\\) returned to (.*)"),
+				(m, ctx, line) -> List.of(Action.of(UNCALLED_BET_RETURNED, ctx.getCurrentStreet(), m.group(3),
+						m.group(1) + m.group(2), Double.parseDouble(m.group(2)), line))));
 
-		rules.add(new ParsingRule(Pattern.compile("^Total pot (\\d+)(?: \\| Rake (\\d+))?"),
-				(m, ctx, line) -> List.of(Action.of(TOTAL_POT, ctx.getCurrentStreet(), null, "Total pot " + m.group(1),
-						Double.parseDouble(m.group(1)), line))));
+		rules.add(new ParsingRule(Pattern.compile("^Total pot (\\$?)([\\d.]+)(?: \\| Rake (\\d+))?"),
+				(m, ctx, line) -> List.of(Action.of(TOTAL_POT, ctx.getCurrentStreet(), null,
+						"Total pot " + m.group(1) + m.group(2), Double.parseDouble(m.group(2)), line))));
 
 		rules.add(new ParsingRule(Pattern.compile("^Board \\[(.*?)]"),
 				(m, ctx, line) -> List.of(Action.of(BOARD, ctx.getCurrentStreet(), null, m.group(1), 0, line))));
