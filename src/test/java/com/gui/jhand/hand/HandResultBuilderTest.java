7486e7491f7bc08c415e83e7b9ddd9b9fc8dc843
GuiRodriguero <gui.rodriguero@hotmail.com>
2026-04-06 02:35:54 -0300

    Add StartingHand converter to HandResult

--- src/test/java/com/gui/jhand/hand/HandResultBuilderTest.java
+++ src/test/java/com/gui/jhand/hand/HandResultBuilderTest.java
@@ -11,6 +11,7 @@ import java.time.Instant;
 import java.util.List;
 
 import static com.gui.jhand.core.poker.HandRank.*;
+import static com.gui.jhand.core.poker.StartingHand.fromHandHistory;
 import static com.gui.jhand.hand.HandTemplateLoader.*;
 import static com.gui.jhand.hand.Position.*;
 import static java.math.BigDecimal.ZERO;
@@ -40,9 +41,9 @@ class HandResultBuilderTest {
 		String hand = HandTemplateLoader.validWonWithShowdown();
 		actions = parser.parse(hand);
 
-		HandResult expected = HandResult.of("259827261990", "My Session", Instant.ofEpochSecond(1771774381), "Qd Jh",
-				UTG, ChipsUtils.of(800.0), true, true, ChipsUtils.of(970.0), ChipsUtils.of(2490.0),
-				ChipsUtils.of(1520.0), PAIR);
+		HandResult expected = HandResult.of("259827261990", "My Session", Instant.ofEpochSecond(1771774381),
+				fromHandHistory("Qd Jh"), UTG, ChipsUtils.of(800.0), true, true, ChipsUtils.of(970.0),
+				ChipsUtils.of(2490.0), ChipsUtils.of(1520.0), PAIR);
 
 		assertThat(builder.build(actions, "My Session", "GuiRodri2013")).isEqualTo(expected);
 	}
@@ -52,8 +53,9 @@ class HandResultBuilderTest {
 		String hand = HandTemplateLoader.validLostWithShowdown();
 		actions = parser.parse(hand);
 
-		HandResult expected = HandResult.of("259827220105", "My Session", Instant.ofEpochSecond(1771774150), "2h Ac",
-				MP, ChipsUtils.of(600.0), true, true, ChipsUtils.of(2555.0), ZERO, ChipsUtils.of(-2555.0), TWO_PAIR);
+		HandResult expected = HandResult.of("259827220105", "My Session", Instant.ofEpochSecond(1771774150),
+				fromHandHistory("2h Ac"), MP, ChipsUtils.of(600.0), true, true, ChipsUtils.of(2555.0), ZERO,
+				ChipsUtils.of(-2555.0), TWO_PAIR);
 
 		assertThat(builder.build(actions, "My Session", "GuiRodri2013")).isEqualTo(expected);
 	}
@@ -63,9 +65,9 @@ class HandResultBuilderTest {
 		String hand = HandTemplateLoader.validWonWithoutShowdown();
 		actions = parser.parse(hand);
 
-		HandResult expected = HandResult.of("259827317520", "My Session", Instant.ofEpochSecond(1771774682), "3d 8c",
-				BB, ChipsUtils.of(1200.0), false, false, ChipsUtils.of(1275.0), ChipsUtils.of(2550.0),
-				ChipsUtils.of(1275.0), null);
+		HandResult expected = HandResult.of("259827317520", "My Session", Instant.ofEpochSecond(1771774682),
+				fromHandHistory("3d 8c"), BB, ChipsUtils.of(1200.0), false, false, ChipsUtils.of(1275.0),
+				ChipsUtils.of(2550.0), ChipsUtils.of(1275.0), null);
 
 		assertThat(builder.build(actions, "My Session", "GuiRodri2013")).isEqualTo(expected);
 	}
@@ -75,9 +77,9 @@ class HandResultBuilderTest {
 		String hand = HandTemplateLoader.validWithSidePot();
 		actions = parser.parse(hand);
 
-		HandResult expected = HandResult.of("259859276702", "My Session", Instant.ofEpochSecond(1771975039), "Ad Ac",
-				UTG, ChipsUtils.of(20.0), true, true, ChipsUtils.of(1494.0), ChipsUtils.of(2263.0),
-				ChipsUtils.of(769.0), HIGH_CARD);
+		HandResult expected = HandResult.of("259859276702", "My Session", Instant.ofEpochSecond(1771975039),
+				fromHandHistory("Ad Ac"), UTG, ChipsUtils.of(20.0), true, true, ChipsUtils.of(1494.0),
+				ChipsUtils.of(2263.0), ChipsUtils.of(769.0), HIGH_CARD);
 
 		assertThat(builder.build(actions, "My Session", "GuiRodri2013")).isEqualTo(expected);
 	}
@@ -87,9 +89,9 @@ class HandResultBuilderTest {
 		String hand = HandTemplateLoader.validWithSidePotAndMultipleWinners();
 		actions = parser.parse(hand);
 
-		HandResult expected = HandResult.of("259859212603", "My Session", Instant.ofEpochSecond(1771974739), "Jc Qc",
-				SB, ChipsUtils.of(50.0), true, false, ChipsUtils.of(950.0), ChipsUtils.of(2850.0),
-				ChipsUtils.of(1900.0), HIGH_CARD);
+		HandResult expected = HandResult.of("259859212603", "My Session", Instant.ofEpochSecond(1771974739),
+				fromHandHistory("Jc Qc"), SB, ChipsUtils.of(50.0), true, false, ChipsUtils.of(950.0),
+				ChipsUtils.of(2850.0), ChipsUtils.of(1900.0), HIGH_CARD);
 
 		assertThat(builder.build(actions, "My Session", "GuiRodri2013")).isEqualTo(expected);
 	}
